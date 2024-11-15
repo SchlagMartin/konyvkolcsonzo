5167cedf398427800fd89e4982444b5cecfe250c
SchlagMartin <schlagm0@gmail.com>
2024-11-15 09:19:18 +0100

    Update KonyvController.php

--- app/Http/Controllers/KonyvController.php
+++ app/Http/Controllers/KonyvController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Konyv;
@@ -19,9 +18,27 @@ class KonyvController extends Controller
         'mufaj' => 'required|string',
         'kiadasev' => 'required|date',
     ]);
-
     Konyv::create($request->all());
 
     return redirect('/konyvek')->with('success', 'Az könyv sikeresen hozzáadva.');
+}
+
+public function index(Request $request)
+{
+    $query = Konyv::query();
+
+    $query->whereDoesntHave('foglalasok', function ($query) {
+        $query->whereNull('rent_end');  
+    });
+
+    $cars = $query->get();
+
+    return view('konyvek.index', compact('konyvek'));
+}
+
+public function show($id)
+{
+    $konyv = Konyv::findOrFail($id);
+    return view('konyv.show', compact('konyv'));
 }
 }
